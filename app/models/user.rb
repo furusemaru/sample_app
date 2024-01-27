5288efcfbe7d56d464a8b50843f0c18e42c5d54f
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-01-27 11:44:21 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
                     uniqueness: true
 
   has_secure_password
-  validates :password, presence:true, length: { minimum: 8 }
+  validates :password, presence:true, length: { minimum: 8}
 end
