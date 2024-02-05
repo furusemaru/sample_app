c01d7564b78d34b926dd7c3ceffc07cf5edf0abc
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-02-05 04:01:54 +0000

    Modify

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+#bundle exec rails db:seed
+bundle exec rails db:migrate
 
